f97b506cc15a0764d43f51a41d1a5dd917d6ce59
Jason Ah Chuen <jahchuen@stanford.edu>
2020-02-28 16:56:49 -0800

    get POST requests working

--- portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentsServlet.java
@@ -24,23 +24,35 @@ import com.google.gson.Gson;
 
 
 
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-
+@WebServlet("/comments")
+public class CommentsServlet extends HttpServlet {
+  private ArrayList<String> comments;
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
-    ArrayList<String> greetings = new ArrayList<String>();
-    greetings.add("Hello");
-    greetings.add("Bonjour");
-    greetings.add("Bonzour");
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // ArrayList to store comments
+    comments = new ArrayList<String>();
+    // Get the input from the form.
+    String text = getParameter(request, "text-input", "");
+    comments.add(text);
 
     Gson gson = new Gson();
-    String json = gson.toJson(greetings);
+    String json = gson.toJson(comments);
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
+
+  /**
+   * @return the request parameter, or the default value if the parameter
+   *         was not specified by the client
+   */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+
 }
-
-
 
