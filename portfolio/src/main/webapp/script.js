b6a076178e2d32e371bee0862063c5e5ab42a83b
Jason Ah Chuen <jahchuen@stanford.edu>
2020-02-28 18:19:02 -0800

    fix minor issue

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -44,7 +44,7 @@ async function getGreeting() {
 
   fetch('/data').then(response => response.json()).then((greetings) => {
     const greetingElem = document.getElementById('greeting-container');
-    var index = Math.floor(Math.random() * 10) % 3;
+    const index = Math.floor(Math.random() * greetings.length)
     greetingElem.innerHTML = greetings[index];
   });
   
